200848284f3934ae576fcf4fdfc33b0a6ffb49b9
Aliaksandr Zhuhrou <aliaksandrzhuhrou@cloud.upwork.com>
2017-05-06 16:13:21 +0300

    extract pam50 data

--- data_loader.py
+++ data_loader.py
@@ -32,13 +32,33 @@ def loadExpressionData():
 
 
 def load_labels_data(sameples_names):
-    expected_labels = []
-    sample_by_sample_name = {}
+    expected_labels = set(['Basal', 'Her2', 'LumA', 'LumB', 'Normal'])
+    label_by_sample_name_raw = {}
     with open('BRCA.datafreeze.20120227.txt', 'rb') as labelsDataFile:
         file_line_number = 1
-        for data_row in labelsDataFile:
+        lines = re.split(r'\r', labelsDataFile.readline())
+        for data_row in lines:
             if file_line_number > 1:
                 data_list = re.split(r'\t', data_row.rstrip('\t\n\r'))
-                
+                sample_name = data_list[0].strip()
+                pam50_classification_label = data_list[3].strip()
+                if pam50_classification_label not in expected_labels:
+                    raise TypeError("line {} of BRCA.datafreeze.20120227.txt contains invalid pam50 type={} for sample name={}".format(file_line_number, pam50_classification_label, sample_name))
+                label_by_sample_name_raw[sample_name] = pam50_classification_label
             file_line_number += 1
-    pass
+
+    label_by_sample_name_filtered = {}
+    for aliquot_barcode in sameples_names:
+        sample_name = convert_aliquot_barcode_to_sample_barcode(aliquot_barcode)
+        if sample_name not in label_by_sample_name_raw:
+            raise RuntimeError("BRCA.datafreeze.20120227.txt doesn't have a pam50 label for a sample name={}".format(sample_name))
+        else:
+            label_by_sample_name_filtered[sample_name] = label_by_sample_name_raw[sample_name]
+
+    return label_by_sample_name_filtered
+
+
+def convert_aliquot_barcode_to_sample_barcode(aliquot_barcode):
+    parts = re.split(r'-', aliquot_barcode)
+    assert len(parts) == 7
+    return "-".join(parts[:4])
--- svm_classifier.py
+++ svm_classifier.py
@@ -1,4 +1,24 @@
 from sklearn import svm
+import numpy
+import data_loader
+import sys
+
+gene_features, samples_names, training_samples = data_loader.loadExpressionData()
+pam50_by_sample_name = data_loader.load_labels_data(samples_names)
+
+sys.stdout.write("Samples size: {} \n".format(len(samples_names)))
+sys.stdout.write("Gene features size: {} \n".format(len(gene_features)))
+print "Samples\Genes\t", "\t".join(gene_features)
+sample_index = 0
+for sample in training_samples:
+    sys.stdout.write(samples_names[sample_index])
+    sys.stdout.write("\t")
+    for feature_index in range(0, len(gene_features)):
+        sys.stdout.write(str(training_samples[sample_index][feature_index]))
+        sys.stdout.write("\t")
+    sys.stdout.write("\n")
+    sample_index += 1
+
 X = [[0, 0], [1, 1]]
 y = [0, 1]
 clf = svm.SVC()
