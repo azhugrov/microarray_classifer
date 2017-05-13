19b94014eeb53e565df8c9580b9bfad6fd507ce8
Aliaksandr Zhuhrou <aliaksandrzhuhrou@cloud.upwork.com>
2017-05-13 20:38:31 +0300

    configure a first iteration of tensfor flow dnn classifier

--- dnn_classifier.py
+++ dnn_classifier.py
@@ -3,9 +3,20 @@ import numpy as np
 import data_loader
 import sys
 
+tf.logging.set_verbosity(tf.logging.INFO)
+
 gene_features, samples_names, training_samples = data_loader.loadExpressionData()
 pam50_by_sample_name = data_loader.load_labels_data(samples_names)
 classes = ['Basal', 'Her2', 'LumA', 'LumB', 'Normal']
+
+training_data_by_gene = {}
+for gene in gene_features:
+    training_data_by_gene[gene] = []
+
+for training_sample in training_samples:
+    for i in range(0, len(gene_features)):
+        gene = gene_features[i]
+        training_data_by_gene.get(gene).append(training_sample[i])
 
 labels = []
 for sample_name in samples_names:
@@ -15,15 +26,21 @@ feature_columns = [tf.contrib.layers.real_valued_column(gene) for gene in gene_f
 
 # Build 3 layer DNN with 10, 20, 10 units respectively.
 classifier = tf.contrib.learn.DNNClassifier(feature_columns=feature_columns,
-                                            hidden_units=[5000, 500, 50],
+                                            hidden_units=[500],
                                             n_classes=5,
                                             model_dir="/tmp/microarray_dnn_1.0")
 
+
 # Define the training inputs
 def get_train_inputs():
-    x = tf.constant(training_samples)
-    y = tf.constant([classes.index(label) for label in labels])
-    return x, y
+    data_tensor_by_features = {k: tf.constant(training_data_by_gene[k.name], shape=(len(training_samples), 1)) for k in feature_columns}
+    y = tf.constant([classes.index(label) for label in labels], shape=(len(training_samples), 1))
+    return data_tensor_by_features, y
 
 # Fit model.
 classifier.fit(input_fn=get_train_inputs, steps=2000)
+
+ev = classifier.evaluate(input_fn=get_train_inputs, steps=1)
+
+print ev
+print "Loss evaluation", ev["loss"]
--- svm_classifier.py
+++ svm_classifier.py
@@ -13,8 +13,6 @@ for sample_name in samples_names:
 # clf = svm.SVC(kernel='linear', C=1)
 # scores = cross_val_score(clf, training_samples, labels, cv=5)
 
-clf = MLPClassifier(solver='adam', alpha=1e-5, hidden_layer_sizes=(5000, 1000, 100, 10), random_state=1, verbose=True)
-clf.fit(training_samples, labels)
-print clf.score(training_samples, labels)
-# scores = cross_val_score(clf, training_samples, labels, cv=5)
-# print "scores: ", scores
+clf = MLPClassifier(solver='adam', alpha=1e-5, hidden_layer_sizes=(5000, 1000), random_state=1, verbose=True)
+scores = cross_val_score(clf, training_samples, labels, cv=5)
+print "scores: ", scores
